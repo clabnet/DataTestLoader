52e0d881354d1607d118174e330ae9f9a5ae371e
Claudio Barca <c.barca@libero.it>
2015-08-19 22:31:55 +0200

    Large refactoring.

--- DataTestLoader/DatabaseScriptManager.cs
+++ DataTestLoader/DatabaseScriptManager.cs
@@ -24,14 +24,12 @@ namespace DataTestLoader
         private string servicePath;
         private string hostName;
         private string psqlExe;
-        private string createdbExe;
         private string pgdumpExe;
 
         private ConnectionParser dbSource;
-
         private ConnectionParser dbTest;
 
-        public const string SQL_CreateExtensionPostgis = @"CREATE EXTENSION postgis;";
+        
 
         public PostgresqlScriptManager(bool refreshSchema, bool initDatabase, bool loadJsonData)
         {
@@ -71,7 +69,7 @@ namespace DataTestLoader
         {
 
             string keyName;
-            
+
             string serviceName = "postgresql-x64-9.4";
 
             // These keys are always required
@@ -89,35 +87,13 @@ namespace DataTestLoader
             if (!ConnectionExist(keyName))
                 throw new ApplicationException("Missing connection string " + keyName + " in configuration file");
 
-            //keyName = "FolderSchema";
-            //if (!ConfigKeyExist(keyName))
-            //    throw new ApplicationException("Missing key " + keyName + " in configuration file");
-
-            //if (!Directory.Exists(FolderSchema))
-            //    Directory.CreateDirectory(FolderSchema);
-
-            //string logPath = Path.Combine(FolderSchema, "log");
-
-            //if (Directory.Exists(logPath))
-            //    Directory.Delete(logPath, true); // clear log smart mode
-
-            //Directory.CreateDirectory(logPath);
-
             servicePath = GetServicePath(serviceName);
             if (string.IsNullOrEmpty(servicePath))
                 throw new ApplicationException(string.Format("Postgres DB service {0} is not installed on server {1}. ", serviceName, dbSource.Server));
 
             psqlExe = Path.Combine(this.servicePath, "psql.exe");
-            if (!File.Exists(psqlExe))
-                throw new FileNotFoundException(string.Format("Not found {0}", psqlExe));
-
-            createdbExe = Path.Combine(this.servicePath, "createdb.exe");
-            if (!File.Exists(createdbExe))
-                throw new FileNotFoundException(string.Format("Not found {0}", createdbExe));
 
             pgdumpExe = Path.Combine(this.servicePath, "pg_dump.exe");
-            if (!File.Exists(pgdumpExe))
-                throw new FileNotFoundException(string.Format("Not found {0}", pgdumpExe));
 
             keyName = "DBSource";
             if (!ConnectionExist(keyName))
@@ -207,11 +183,10 @@ namespace DataTestLoader
             string arguments = String.Format(@" --host {0} --port {1} --username {2} --schema-only --section=pre-data --no-owner --no-privileges --encoding UTF8 --file {3} --dbname {4}", dbSource.Server, dbSource.Port, dbSource.Username, fullPath, dbSource.Database);
 
             ProcessStartInfo processInfo = CreateProcessInfo(pgdumpExe, arguments);
-            string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-CreateSchema-PREDATA.log");
 
             logger.Info(string.Format("Waiting please, retrieving DB schema PRE-DATA from {0} may take up two minutes ", dbSource.Server));
 
-            RunProcess(processInfo, errorFile);
+            RunProcess(processInfo);
 
             this.FileSchemaPreData = fullPath;
 
@@ -233,11 +208,10 @@ namespace DataTestLoader
             string arguments = String.Format(@" --host {0} --port {1} --username {2} --schema-only --section=post-data --no-owner --no-privileges --encoding UTF8 --file {3} --dbname {4}", dbSource.Server, dbSource.Port, dbSource.Username, fullPath, dbSource.Database);
 
             ProcessStartInfo processInfo = CreateProcessInfo(pgdumpExe, arguments);
-            string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-CreateSchema-POSTDATA.log");
 
             logger.Info(string.Format("Waiting please, retrieving DB schema POSTDATA from {0} may take up two minutes ", dbSource.Server));
 
-            RunProcess(processInfo, errorFile);
+            RunProcess(processInfo);
 
             this.FileSchemaPostData = fullPath;
 
@@ -259,142 +233,77 @@ namespace DataTestLoader
 
         private void DropAllConnections()
         {
-            try
-            {
-                // valid for v.9.2 and above (else use procpid in substitution of pid)
-                string psqlCmd = "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid()";
+            // valid for v.9.2 and above (else use procpid in substitution of pid)
+            string psqlCmd = "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid()";
 
-                // use psql command
-                string arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {4} --command ""{3}"" ", dbTest.Server, dbTest.Port, dbTest.Username, psqlCmd, dbTest.Database);
+            string arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {4} --command ""{3}"" ", dbTest.Server, dbTest.Port, dbTest.Username, psqlCmd, dbTest.Database);
 
-                ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, arguments);
+            ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, arguments);
 
-                string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-DropAllConnections.log");
+            RunProcess(processInfo);
 
-                RunProcess(processInfo, errorFile);
-
-                logger.Info(string.Format("Dropped all connections to database {0}", dbTest.Database));
-
-            }
-
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                throw;
-            }
+            logger.Info(string.Format("Dropped all connections to database {0}", dbTest.Database));
         }
 
         private void CreateExtensionPostgis()
         {
-            try
-            {
-                string psqlCmd = SQL_CreateExtensionPostgis;
+            string psqlCmd = @"CREATE EXTENSION postgis;";
 
-                // use psql command
-                string arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {4} --command ""{3}"" ", dbTest.Server, dbTest.Port, dbTest.Username, psqlCmd, dbTest.Database);
+            string arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {4} --command ""{3}"" ", dbTest.Server, dbTest.Port, dbTest.Username, psqlCmd, dbTest.Database);
 
-                ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, arguments);
+            ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, arguments);
 
-                string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-CreateExtensionPostgis.log");
+            RunProcess(processInfo);
 
-                RunProcess(processInfo, errorFile);
-
-                logger.Info(string.Format("Created extension Postgis", dbTest.Database));
-
-            }
-
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                throw;
-            }
+            logger.Info(string.Format("Created extension Postgis", dbTest.Database));
         }
 
         private void DropDatabase()
         {
-            try
-            {
-                string dropdbExe = Path.Combine(this.servicePath, "dropdb.exe");
+            string dropdbExe = Path.Combine(this.servicePath, "dropdb.exe");
 
-                if (!File.Exists(dropdbExe))
-                    throw new FileNotFoundException(string.Format("Not found {0}", dropdbExe));
+            string arguments = String.Format(@" --host {0} --port {1} --username {2} {3}", dbTest.Server, dbTest.Port, dbTest.Username, dbTest.Database);
 
-                // Step 2 : drop database
-                string arguments = String.Format(@" --host {0} --port {1} --username {2} {3}", dbTest.Server, dbTest.Port, dbTest.Username, dbTest.Database);
+            ProcessStartInfo processInfo = CreateProcessInfo(dropdbExe, arguments);
 
-                ProcessStartInfo processInfo = CreateProcessInfo(dropdbExe, arguments);
+            RunProcess(processInfo);
 
-                string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-DropDatabase.log");
-
-                RunProcess(processInfo, errorFile);
-
-                logger.Info(string.Format("Dropped database {0}", dbTest.Database));
-
-            }
-            catch (Exception ex)
-            {
-                // only debug message
-                logger.Error(ex);
-            }
-
+            logger.Info(string.Format("Dropped database {0}", dbTest.Database));
         }
 
         private void CreateDatabase()
         {
-            try
-            {
-                // use createDb command
+            string createdbExe = Path.Combine(this.servicePath, "createdb.exe");
 
-                string arguments = String.Format(@" --host {0} --port {1} --username {2} {3}", dbTest.Server, dbTest.Port, dbTest.Username, dbTest.Database);
+            string arguments = String.Format(@" --host {0} --port {1} --username {2} {3}", 
+                dbTest.Server, dbTest.Port, dbTest.Username, dbTest.Database);
 
-                ProcessStartInfo processInfo = CreateProcessInfo(createdbExe, arguments);
+            ProcessStartInfo processInfo = CreateProcessInfo(createdbExe, arguments);
 
-                string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-CreateDatabase.log");
+            RunProcess(processInfo);
 
-                RunProcess(processInfo, errorFile);
-
-                logger.Info(string.Format("Created database {0}", dbTest.Database));
-
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                throw;
-            }
-
+            logger.Info(string.Format("Created database {0}", dbTest.Database));
         }
 
-        // PRIMA 
-        //private static void RunProcess(ProcessStartInfo processInfo, string errorFile)
-        //{
-        //    using (Process process = Process.Start(processInfo))
-        //    {
-        //        using (StreamReader reader = process.StandardError)
-        //        {
-        //            StreamWriter sw = new StreamWriter(errorFile);
-        //            sw.WriteLine(reader.ReadToEnd());
-        //            sw.Close();
-        //        }
-        //    }
-        //}
-
-        //TODO : restituire log su Nlog
-        private static void RunProcess(ProcessStartInfo processInfo, string errorFile)
+        private static void RunProcess(ProcessStartInfo processInfo)
         {
             using (Process process = Process.Start(processInfo))
             {
-                using (StreamReader reader = process.StandardError)
+                StreamReader err = process.StandardError;
+                string errorMessage = err.ReadToEnd();
+                if (errorMessage != string.Empty)
                 {
-                    StreamWriter sw = new StreamWriter(errorFile);
-                    sw.WriteLine(reader.ReadToEnd());
-                    sw.Close();
+                    logger.Fatal(errorMessage);
+                    throw new ApplicationException(errorMessage);
                 }
             }
         }
 
-
         private ProcessStartInfo CreateProcessInfo(string exeName, string arguments)
         {
+            if (!File.Exists(exeName))
+                throw new FileNotFoundException(string.Format("Not found {0}", exeName));
+
             logger.Debug(exeName + arguments);
 
             ProcessStartInfo processInfo = new ProcessStartInfo
@@ -457,41 +366,17 @@ namespace DataTestLoader
 
         private void RunPsqlScript(string scriptName)
         {
-            string fileName, arguments;
-            bool result;
+            string fileName = Path.Combine(this.FolderSchema, scriptName);
 
-            fileName = Path.Combine(this.FolderSchema, scriptName);
             if (!File.Exists(fileName))
                 throw new FileNotFoundException(string.Format("Not found {0}", fileName));
 
-            arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {3} --file ""{4}""",
+            string arguments = String.Format(@" --host {0} --port {1} --username {2} --dbname {3} --file ""{4}""",
                 dbTest.Server, dbTest.Port, dbTest.Username, dbTest.Database, fileName);
 
-            result = ExecPsqlCommand(arguments);
-        }
+            ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, arguments);
 
-        private bool ExecPsqlCommand(string psqlArguments)
-        {
-
-            try
-            {
-                // use psql command
-
-                ProcessStartInfo processInfo = CreateProcessInfo(psqlExe, psqlArguments);
-
-                string errorFile = Path.Combine(this.FolderSchema, @"log\DTL-ExecScripts.log");
-
-                RunProcess(processInfo, errorFile);
-
-                return true;
-
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-                throw;
-            }
-
+            RunProcess(processInfo);
         }
 
         private string GetMachineNameFromIPAddress(string ipAdress)
@@ -517,24 +402,18 @@ namespace DataTestLoader
             {
                 if (svc.ServiceName.Contains(serviceName))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("  Service :        {0}", svc.ServiceName);
-                    Console.WriteLine("    Display name:    {0}", svc.DisplayName);
+                    logger.Info("Found Service {0}", svc.DisplayName);
 
                     ManagementObject wmiService = new ManagementObject("Win32_Service.Name='" + svc.ServiceName + "'");
 
                     wmiService.Get();
-
-                    Console.WriteLine("    Start name:      {0}", wmiService["StartName"]);
-                    Console.WriteLine("    Description:     {0}", wmiService["Description"]);
-                    Console.WriteLine("    PathName   :     {0}", wmiService["PathName"]);
 
                     string pathName = wmiService["PathName"].ToString();
                     int end = pathName.IndexOf(".exe");
                     string serviceExecutable = pathName.Substring(1, end + 3);
                     string pathExecutable = Path.GetDirectoryName(serviceExecutable);
 
-                    Console.WriteLine("    PathExe    :     {0}", pathExecutable);
+                    logger.Debug("PathExe :{0}", pathExecutable);
 
                     return pathExecutable;
                 }
